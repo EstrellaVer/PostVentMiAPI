6a42ed9071bd37a6a0cb78c029a13a56252e8392
3str3ll4V3r <156131289+3str3ll4V3r@users.noreply.github.com>
2025-08-19 19:44:09 -0600

    Cambios en los datos que se piden

--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -5,7 +5,7 @@ namespace MiApi.Models
         public int Id { get; set; }            // Clave primaria
         public string Nombre { get; set; } = null!;
         public string Email { get; set; } = null!;
-        public string Passwprd { get; set; } = null!;
+        public string Password { get; set; } = null!;
         public string Puesto { get; set; } = null!;
         public string Empresa { get; set; } = null!;
     }
